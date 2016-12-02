31687ab782f87c480d30d114687d9f831f599f7f
Brais Núñez <Brais Nez@DESKTOP-EI9I1J1>
2016-12-02 11:25:50 +0100

    Programa calcular tiempo frase

---
+++ src/boletin11/programacionej2/Boletin11ProgramacionEj2.java
@@ -0,0 +1,17 @@
+
+package boletin11.programacionej2;
+
+/**
+ *
+ * @author Brais Núñez
+ */
+public class Boletin11ProgramacionEj2 {
+
+    public static void main(String[] args) {
+       
+        TiempoFrase frase1 = new TiempoFrase ();
+        frase1.tiempoFrase();
+        
+    }
+    
+}
---
+++ src/boletin11/programacionej2/TiempoFrase.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin11.programacionej2;
+
+import java.time.Duration;
+import java.time.Instant;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brais Núñez
+ */
+public class TiempoFrase {
+    
+    public void tiempoFrase (){
+        
+        Instant ahora = Instant.now();
+        JOptionPane.showInputDialog("Escribe a frase");
+        Instant antes = Instant.now();
+        Duration tiempo = Duration.between(ahora, antes);
+        
+        System.out.println("Ha tardado en escribir la frase " + tiempo.getSeconds() + " segundos");
+    }
+    
+}
